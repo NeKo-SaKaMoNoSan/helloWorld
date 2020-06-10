7f8cea46398f60a9cc1eb62e1d818ba707afacd3
NeKo <NeKo@192.168.48.1>
2020-06-10 16:06:12 +0800

    修改

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -2,6 +2,6 @@ package helloworld;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("hello world!---neko");
+		System.out.println("hello world!---neko随便改一改");
 	}
 }
